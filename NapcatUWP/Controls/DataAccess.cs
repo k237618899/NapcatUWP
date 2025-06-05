7dcbe6cbc6619293ce4ddbcdc3fb92767d18e23d
k237618899 <53432315+k237618899@users.noreply.github.com>
2025-06-05 16:41:17 +0800

    添加项目文件。

---
+++ NapcatUWP/Controls/DataAccess.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using Windows.Storage;
+using Microsoft.Data.Sqlite;
+
+namespace NapcatUWP.Controls
+{
+    public class DataAccess
+    {
+        public static async void InitializeDatabase()
+        {
+            await ApplicationData.Current.LocalFolder.CreateFileAsync("setting.db",
+                CreationCollisionOption.OpenIfExists);
+            var dbpath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "setting.db");
+            using (var db =
+                   new SqliteConnection($"Filename={dbpath}"))
+            {
+                db.Open();
+
+                var tableCommand = "CREATE TABLE IF NOT " +
+                                   "EXISTS AppSettings (Primary_Key NVARCHAR(2048) PRIMARY KEY, " +
+                                   "Text_Entry NVARCHAR(2048) NULL)";
+
+                var createTable = new SqliteCommand(tableCommand, db);
+
+                createTable.ExecuteReader();
+            }
+        }
+
+        public static void InitInsert()
+        {
+            Insert("Server", "http://140.83.32.184:3000");
+            Insert("Account","");
+            Insert("Token","");
+        }
+
+        public static void Insert(String inputKey, String inputText)
+        {
+            if (inputKey == null || inputKey.Equals(String.Empty))
+            {
+                return;
+            }
+            string dbpath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "setting.db");
+            using (SqliteConnection db =
+                   new SqliteConnection($"Filename={dbpath}"))
+            {
+                db.Open();
+
+                SqliteCommand insertCommand = new SqliteCommand();
+                insertCommand.Connection = db;
+
+                // Use parameterized query to prevent SQL injection attacks
+                insertCommand.CommandText = "INSERT or IGNORE INTO AppSettings VALUES (@Key, @Entry);";
+                insertCommand.Parameters.AddWithValue("@Key",inputKey);
+                insertCommand.Parameters.AddWithValue("@Entry", inputText);
+
+                insertCommand.ExecuteReader();
+            }
+        }
+        public static NameValueCollection GetAllDatas()
+        {
+            NameValueCollection entries = new NameValueCollection();
+            
+            string dbpath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "setting.db");
+            using (SqliteConnection db =
+                   new SqliteConnection($"Filename={dbpath}"))
+            {
+                db.Open();
+
+                SqliteCommand selectCommand = new SqliteCommand
+                    ("SELECT * from AppSettings", db);
+
+                SqliteDataReader query = selectCommand.ExecuteReader();
+
+                while (query.Read())
+                {
+                    entries.Add(query.GetString(0),query.GetString(1));
+                }
+            }
+            return entries;
+        }
+
+    }
+}
---
+++ NapcatUWP/MainPage.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Specialized;
+using Windows.Foundation;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using NapcatUWP.Controls;
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace NapcatUWP
+{
+    /// <summary>
+    ///     可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public string ConnectionAddr = "http://140.83.32.184:3000";
+        private NameValueCollection _settingsCollection = new NameValueCollection();
+
+        public MainPage()
+        {
+            InitializeComponent();
+            InitializeDB();
+            UpdatePageAndSetting();
+        }
+
+        private void UpdatePageAndSetting()
+        {
+            ConnectionAddr = _settingsCollection.Get("Server");
+            TextBoxAccount.Text = _settingsCollection.Get("Account");
+            PasswordBoxToken.Password = _settingsCollection.Get("Token");
+        }
+        private void InitializeDB()
+        {
+            IAsyncAction asyncAction = Windows.System.Threading.ThreadPool.RunAsync((workItem) =>
+            {
+                DataAccess.InitializeDatabase();
+                DataAccess.InitInsert();
+                _settingsCollection = DataAccess.GetAllDatas();
+            });
+
+        }
+        /// <summary>
+        ///     Handles the Click event of the SettingBars control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="RoutedEventArgs" /> instance containing the event data.</param>
+        private async void SettingBars_Click(object sender, RoutedEventArgs e)
+        {
+            var btn = sender as Button;
+            var dialog = new ContentDialog
+            {
+                Title = "NapCat Settings",
+                RequestedTheme = ElementTheme.Dark,
+                //FullSizeDesired = true,
+                MaxWidth = ActualWidth // Required for Mobile!
+            };
+            var panel = new StackPanel();
+            var ipAddr = new TextBox
+            {
+                Header = "HTTP Server Address"
+            };
+            if (ConnectionAddr != string.Empty) ipAddr.Text = ConnectionAddr;
+
+
+            panel.Children.Add(ipAddr);
+            dialog.Content = panel;
+            // Add Buttons
+            dialog.PrimaryButtonText = "Save";
+            dialog.PrimaryButtonClick += delegate
+            {
+                var isURL = AddressCheck(ipAddr.Text);
+                if (isURL)
+                {
+                    ConnectionAddr = ipAddr.Text;
+                    btn.Content = "Setting Saved";
+                }
+                else
+                {
+                    new MessageDialog("Not a valid URL!", "Warning").ShowAsync();
+                }
+            };
+
+            dialog.SecondaryButtonText = "Cancel";
+            dialog.SecondaryButtonClick += delegate { btn.Content = "Cancel"; };
+
+            // Show Dialog
+            var result = await dialog.ShowAsync();
+            if (result == ContentDialogResult.None)
+                btn.Content = "Result: NONE";
+            else
+                btn.Content = "Settings";
+        }
+
+        /// <summary>
+        ///     Addresses the check.
+        /// </summary>
+        /// <param name="text">The text.</param>
+        /// <returns>isURL</returns>
+        private bool AddressCheck(string text)
+        {
+            var isUri = Uri.IsWellFormedUriString(text, UriKind.Absolute);
+            return isUri;
+        }
+    }
+}
